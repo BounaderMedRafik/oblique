1dc48e92d9c96d55351d77714360d630c64f6f6d
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-08-16 18:16:32 +0100

    updating stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/marketing/Footer";
 import Hero from "@/components/marketing/Hero";
-import Navbar from "@/components/marketing/Navbar";
 import WhoOblique from "@/components/marketing/WhoOblique";
 import WhyOblique from "@/components/marketing/WhyOblique";
 
